d48652d3f2ca4c7742462c54598263c2f89b6b7f
Raymond Duong <iiraay@hotmail.com>
2024-08-18 21:38:11 -0600

    template and title

---
+++ src/components/header.tsx
@@ -0,0 +1,10 @@
+const FruitList: React.FC = () => {
+    return (
+      <div className="p-4">
+        <h1 className="text-2xl font-bold mb-4">Fruit List App - Finofo!</h1>
+      </div>
+    );
+  }
+  
+  
+  export default FruitList;
